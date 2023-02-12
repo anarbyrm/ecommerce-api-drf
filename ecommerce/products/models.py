a5a0f1c6008a648904f552c6604cfa0845bf1619
Anar Bairam <anar.bayram@yahoo.com>
2023-02-12 21:07:44 +0400

    product model extended

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -10,13 +10,19 @@ class Product(models.Model):
     name = models.CharField(max_length=225)
     description = models.TextField()
     image = models.ImageField(upload_to='product/')
+    color = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     slug = models.SlugField(null=True, blank=True, unique=True)
+    in_stock = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    updated_at = models.DateTimeField(auto_now=True)
+    
     def __str__(self):
         return self.title
     
+    class Meta:
+        ordering = ('-updated_at', '-created_at')
+        
 
 class Cart(models.Model):
     uuid = models.UUIDField(default=uuid4, primary_key=True)
@@ -43,7 +49,7 @@ class CartItem(models.Model):
     
     class Meta:
         unique_together = ('cart', 'product')
-        ordering = ('updated_at',)
+        ordering = ('-updated_at',)
     
     def __str__(self):
         return f'{self.cart}: {self.product} x {self.quantity}'
